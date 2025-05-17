d944dad9b6fed77f02d723c661d47147fb2d49cb
stargazerZJ <53366576+stargazerZJ@users.noreply.github.com>
2025-05-17 07:54:00 +0000

    feat: repeat rollout in main loop instead of in vllm

--- verl/trainer/ppo/ray_trainer.py
+++ verl/trainer/ppo/ray_trainer.py
@@ -898,6 +898,8 @@ class RayPPOTrainer:
                     batch_keys=batch_keys_to_pop,
                     non_tensor_batch_keys=non_tensor_batch_keys_to_pop,
                 )
+                # repeat the gen batch here instead of during rollout
+                gen_batch = gen_batch.repeat(repeat_times=self.config.actor_rollout_ref.rollout.n, interleave=True)
 
                 is_last_step = self.global_steps >= self.total_training_steps
 
