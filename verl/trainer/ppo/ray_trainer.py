refactor: no longer shift response into the prompt input tensor fix response mask computation

verl/trainer/ppo/ray_trainer.py
@@ -894,12 +894,13 @@ class RayPPOTrainer:
                 # repeat to align with repeated responses in rollout
                 batch = batch.repeat(repeat_times=self.config.actor_rollout_ref.rollout.n, interleave=True)
                 batch.non_tensor_batch["age"] = np.ones(len(batch.batch), dtype=int)
+                batch.non_tensor_batch["raw_response_ids"] = np.array([[] for _ in range(len(batch.batch))], dtype=object)
 
                 batch = DataProto.concat([batch, partial_batch])
 
                 # pop those keys for generation
                 batch_keys_to_pop = ["input_ids", "attention_mask", "position_ids"]
-                non_tensor_batch_keys_to_pop = ["raw_prompt_ids"]
+                non_tensor_batch_keys_to_pop = ["raw_prompt_ids", "raw_response_ids"]
                 if "multi_modal_inputs" in batch.non_tensor_batch:
                     non_tensor_batch_keys_to_pop.extend(["multi_modal_data", "multi_modal_inputs"])
                 if "raw_prompt" in batch.non_tensor_batch:
@@ -924,31 +925,15 @@ class RayPPOTrainer:
                         finished_mask = batch.non_tensor_batch.pop("finished")
                         finished_mask = (batch.non_tensor_batch["age"] == max_age) | finished_mask
                         staged_out, partial_batch = DataProto.split(batch, finished_mask)
-                        staged_out.non_tensor_batch.pop("raw_seq_ids")
+                        staged_out.non_tensor_batch.pop("raw_prompt_ids")
+                        staged_out.non_tensor_batch.pop("raw_response_ids")
 
                         partial_batch.non_tensor_batch["age"] += 1
 
                         if len(partial_batch.batch) > 0:
-                            # concatenate the responses in partial batch back to prompts.
-                            b = len(partial_batch.batch)
-                            raw_seq_ids = partial_batch.non_tensor_batch.pop("raw_seq_ids")
-                            new_prompt_lengths = torch.tensor(
-                                [len(seq) for seq in raw_seq_ids]
-                            )
-                            max_prompt_length = self.config.data.max_prompt_length
-                            assert new_prompt_lengths.max() <= max_prompt_length, \
-                                f"max_prompt_length {new_prompt_lengths.max()} exceeds original max_prompt_length {max_prompt_length}"
-
-                            attention_mask = torch.zeros((b, max_prompt_length), dtype=torch.int64)
-                            attention_mask[torch.arange(b), max_prompt_length - new_prompt_lengths] = 1
-                            attention_mask = attention_mask.cumsum(dim=1)
-                            partial_batch.batch["attention_mask"] = attention_mask
-                            partial_batch.batch["position_ids"] = attention_mask.cumsum(dim=1) - 1
-                            partial_batch.batch["input_ids"] = torch.zeros((b, max_prompt_length), dtype=torch.int64)
-                            # input_ids is not actually used in the generation, so we can set it to zeros
-
-                            raw_seq_ids = np.array([[x if x < 151669 else 0 for x in row] for row in raw_seq_ids], dtype=object)
-                            partial_batch.non_tensor_batch["raw_prompt_ids"] = raw_seq_ids
+                            for key in ("input_ids", "attention_mask", "position_ids"):
+                                tmp = partial_batch.batch.pop(key, None)
+                                partial_batch.batch[key] = tmp[:, :self.config.data.max_prompt_length]
 
                             for key in ("prompts", "responses"):
                                 partial_batch.batch.pop(key)

verl/workers/rollout/vllm_rollout/vllm_rollout_spmd.py
@@ -217,13 +217,16 @@ class vLLMRollout(BaseRollout):
             raise RuntimeError("vllm sharding manager is not work properly.")
 
         raw_prompt_ids = non_tensor_batch.pop("raw_prompt_ids")
+        raw_response_ids = non_tensor_batch.pop("raw_response_ids")
+        max_age = 2 # max rounds of rollout before the prompt is forced finished
 
         if "multi_modal_data" in non_tensor_batch:
             vllm_inputs = []
             for raw_prompt_ids, multi_modal_data in zip(non_tensor_batch.pop("raw_prompt_ids"), non_tensor_batch.pop("multi_modal_data")):
                 vllm_inputs.append({"prompt_token_ids": raw_prompt_ids, "multi_modal_data": multi_modal_data})
         else:
-            vllm_inputs = [{"prompt_token_ids": raw_prompt_ids_} for raw_prompt_ids_ in raw_prompt_ids]
+            vllm_inputs = [{"prompt_token_ids": raw_prompt_ids_ + raw_response_ids_}
+                           for raw_prompt_ids_, raw_response_ids_ in zip(raw_prompt_ids, raw_response_ids)]
 
         # ensure the type of `prompt_token_ids` passed to vllm is list[int]
         # https://github.com/volcengine/verl/pull/772
@@ -255,6 +258,7 @@ class vLLMRollout(BaseRollout):
         else:
             kwargs = {
                 "n": 1, # also repeated in ray_trainer
+                "max_tokens" : self.config.response_length // max_age,
             }
 
         # users can customize different sampling_params at different run
@@ -272,12 +276,14 @@ class vLLMRollout(BaseRollout):
             finished = []
             for output in outputs:
                 for completion in output.outputs:
-                    response.append(completion.token_ids)
+                    filtered_response = [id if id < 151669 else 0 for id in completion.token_ids]
+                    response.append(filtered_response)
                     finished.append(completion.finish_reason != "length")
             non_tensor_batch["finished"] = np.array(finished)
-            non_tensor_batch["raw_seq_ids"] = raw_prompt_ids + np.array(response, dtype=object)
+            non_tensor_batch["raw_response_ids"] = np.array(response, dtype=object)
+            non_tensor_batch["raw_prompt_ids"] = raw_prompt_ids
 
-            response = pad_2d_list_to_length(response, self.pad_token_id, max_length=self.config.response_length).to(idx.device)
+            response = pad_2d_list_to_length(raw_response_ids + response, self.pad_token_id, max_length=self.config.response_length).to(idx.device)
 
             if self.sampling_params.n > 1 and do_sample:
                 idx = _repeat_interleave(idx, self.sampling_params.n)